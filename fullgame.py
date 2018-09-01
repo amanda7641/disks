fbdd512e498b21d09603f05e85ce1bc246f0240d
Amanda Owens <amanda7641@gmail.com>
2018-09-01 17:02:21 -0500

    trying to make it run faster

--- fullgame.py
+++ fullgame.py
@@ -1,5 +1,8 @@
 import itertools
 
+from datetime import datetime
+startTime = datetime.now()
+
 p = input("What is your P value: ")
 q = input("What is your Q value: ")
 p = int(p)
@@ -14,6 +17,37 @@ print(diskNumbers)
 diskChoices = list(itertools.combinations(range(p*q), disks))
 numberOfDiskChoices = len(diskChoices)
 print("Original # of diskChoices: " + str(numberOfDiskChoices))
+
+# count = 0
+# while count < len(diskChoices)-(p*q)+2:
+#     whereWShouldStart = count+1
+#     for l in range(1, len(diskNumbers)):
+#         tempList = []
+#         for m in range(disks):
+#             temp = (diskChoices[count][m]+l)%(p*q)
+#             tempList.append(temp)
+#         tempList = sorted(tempList)
+#         n = whereWShouldStart
+#         if n >= len(diskChoices):
+#             n = count+1
+#         for s in range((len(diskChoices)-count)):
+#             match = True
+#             r=0
+#             while r < len(tempList):
+#                 if diskChoices[n][r] == tempList[r]:
+#                     r = r+1
+#                     continue
+#                 else:
+#                     match = False
+#                 r = r+1
+#             if match:    
+#                 diskChoices.pop(n)
+#                 whereWShouldStart=n#
+#                 break
+#             n = n+1
+#             if n >= len(diskChoices):
+#                 n = count+1
+#     count = count+1
 
 count = 0
 while count < len(diskChoices)-(p*q)+2:
@@ -36,6 +70,7 @@ while count < len(diskChoices)-(p*q)+2:
                 r = r+1
             if match:    
                 diskChoices.pop(n)
+                break
             n = n+1
     count = count+1
 
@@ -53,3 +88,5 @@ for a in range(len(diskChoices)):
 # for item in diskChoices:
 #     print(item)
 print("New # of diskChoices: " + str(len(diskChoices)))
+
+print("Runtime: " + str(datetime.now()-startTime))
